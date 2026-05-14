babcd87d142e9547f9c34c96a607c7b500971e31
PEDRO HENRIQUE DOS REIS MARQUES <pedro.hrmarques2@senacsp.edu.br>
2026-05-14 16:56:55 -0300

    Atualização

--- front-end/sorteio/sortear.js
+++ front-end/sorteio/sortear.js
@@ -1,5 +1,23 @@
-function sortear () {
+function sortear() {
 
+    // Agora miramos no quadrado branco (display-container) e não no traço
+    const display = document.getElementById('display-container'); 
     
+    let contador = 0;
+    const intervalo = setInterval(() => {
+        
+        display.innerHTML = `<span class="numero-final" style="font-size: 4rem; opacity: 0.5;">${Math.floor(Math.random() * 99)}</span>`;
+        contador++;
+        
+        if (contador > 15) {
 
+            clearInterval(intervalo);
+            const resultado = Math.floor(Math.random() * 100) + 1;
+            
+            display.innerHTML = `<span class="numero-final">${resultado}</span>`;
+
+        }
+
+    }, 50);
+    
 }
